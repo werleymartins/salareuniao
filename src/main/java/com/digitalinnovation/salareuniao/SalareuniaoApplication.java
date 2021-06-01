0ab33446f3aebb364d4fe3f79f0b33d5ed03aa43
werleymartins <werleymartins@yahoo.com>
2021-05-31 22:30:12 -0300

    Criando projeto

---
+++ src/main/java/com/digitalinnovation/salareuniao/SalareuniaoApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.salareuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalareuniaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalareuniaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/salareuniao/SalareuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.salareuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalareuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
